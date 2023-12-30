9a04298ea37f4b0ac42521d2de26f626e08dd8e1
Debasishtewary05 <155243463+Debasishtewary05@users.noreply.github.com>
2023-12-30 23:04:39 +0530

    #include<stdio.h> #include<stdlib.h> int main(){     int i,n,sum=0,avg;     int a[100];     int count;     scanf("%d",&n);     for(i=0;i<n;i++){         scanf("%d",&a[i]);     }     for(i=0;i<n;i++){         sum +=a[i];         }     avg=sum/n;     for(i=0;i<n;i++){         if(a[i]<=avg)         {             count++;         }     }     printf("%d",count); }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int i,n,sum=0,avg;
+    int a[100];
+    int count;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+        sum +=a[i];
+        }
+    avg=sum/n;
+    for(i=0;i<n;i++){
+        if(a[i]<=avg)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}
