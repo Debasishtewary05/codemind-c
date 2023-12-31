52a5745b328e4f5e2b2648131ab526e1aec2d401
Debasishtewary05 <155243463+Debasishtewary05@users.noreply.github.com>
2023-12-31 22:43:29 +0530

    #include<stdio.h> int main(){     int n,i,j,k,m;     scanf("%d",&n);    char ch=(char)64+(char)(n);     for(i=0;i<n;i++){         for(j=0;j<i;j++){             printf(" ");         }         for(k=0;k<n-i;k++){      printf("%c ",ch);            }         printf(" ");         ch--;     } }

---
+++ Pattern_7.4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n,i,j,k,m;
+    scanf("%d",&n);
+   char ch=(char)64+(char)(n);
+    for(i=0;i<n;i++){
+        for(j=0;j<i;j++){
+            printf(" ");
+        }
+        for(k=0;k<n-i;k++){
+     printf("%c ",ch);
+           }
+        printf("
+");
+        ch--;
+    }
+}
