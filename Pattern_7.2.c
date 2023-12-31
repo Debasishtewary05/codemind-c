dd4570f0d96b79631b3d32ca07d609b9b47efa1d
Debasishtewary05 <155243463+Debasishtewary05@users.noreply.github.com>
2023-12-31 22:25:55 +0530

    #include<stdio.h> int main(){     int n,i,j,k,m;     scanf("%d",&n);     m=n;     for(i=0;i<n;i++){         for(j=0;j<i;j++){             printf(" ");         }         for(k=0;k<n-i;k++){             printf("%d ",m);         }         printf(" ");         m--;     } }

---
+++ Pattern_7.2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n,i,j,k,m;
+    scanf("%d",&n);
+    m=n;
+    for(i=0;i<n;i++){
+        for(j=0;j<i;j++){
+            printf(" ");
+        }
+        for(k=0;k<n-i;k++){
+            printf("%d ",m);
+        }
+        printf("
+");
+        m--;
+    }
+}
